b00771144686674e11bc32e026326c5c873fa781
Rohit Mahajan <tfrohit02@gmail.com>
2026-03-05 17:29:41 +0530

    add Sets.py file with examples of sets and dictionaries

---
+++ Sets.py
@@ -0,0 +1,25 @@
+# set is a collection of unorderedlist
+#indexing is not available 
+
+s = {10, 5, 6, 53, 120, 12}
+print(s)
+
+s = {10, 8, 77, 20, 6}
+lst = [10, 5, 123, 5987]
+
+a = set(lst)
+
+print(a)
+
+#dictionary is also called as mapping data type
+
+d = {10:"unwired", 11:"wired"}
+print(d)
+
+print(d[10])
+
+di = {1:"one",
+      2:"Two",
+      3:"Three"}
+
+print(di[1])
