41e8af00a4d66eaae24cee75a0394ede56227897
Rohit Mahajan <tfrohit02@gmail.com>
2026-03-05 17:03:19 +0530

    refactor Sequences.py to improve formatting and readability

--- Sequences.py
+++ Sequences.py
@@ -9,3 +9,21 @@ my_list = ["a", "b", "s","12", "10.5", "True"]
 
 print(my_list)
 print(my_list[2])
+
+print(my_list[0])
+
+#Tuples contains elements like str, int, float
+
+tp = (15, "str", 10.5)
+
+print(tp[0])
+
+# Range is used to print till nth no.
+# range(start,stop,step)
+r = range(1,10,2)
+q= list(r)
+print(q)
+
+r = list(range(10,50,3))
+
+print(r)
