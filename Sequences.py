a3a6bc241ee01400e7f6ca8ccd3c742e42f3a7fd
Rohit Mahajan <tfrohit02@gmail.com>
2026-03-05 14:20:33 +0530

    add Sequences.py file with string and list examples

---
+++ Sequences.py
@@ -0,0 +1,11 @@
+# Sequences consist: string, list, tuple, range
+
+my_name = "XYZ" #string
+print(my_name)
+
+#list can store diff element
+
+my_list = ["a", "b", "s","12", "10.5", "True"]
+
+print(my_list)
+print(my_list[2])
