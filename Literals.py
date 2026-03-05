c47e9de404efae1d60b60f8904f33c4605633a6d
Rohit Mahajan <tfrohit02@gmail.com>
2026-03-05 19:36:25 +0530

    add Literals.py and operators.py files with examples of literals and operators

---
+++ Literals.py
@@ -0,0 +1,6 @@
+#Literals are 
+
+a = 15 #ts are Int literal
+b = 5.3 #ts are float literal
+c = 0b110101
+print(c)
---
+++ operators.py
@@ -0,0 +1,54 @@
+#there are 5 operators : arithmetic, assignment,
+#  unary minus, relational, logical, boolean
+
+#Arithmetic
+
+a = 5
+b = 2
+
+print(a + b)
+print(a - b)
+print(a * b)
+print(a // b)
+print(a % b)
+
+print(a ** b)
+
+c = 5 ** 2
+print(c)
+
+#assignment
+
+x = 10
+y = 12
+z = x + y
+
+print(z)
+
+#unary minus
+n = 10
+
+print(-n)
+
+m = -20
+
+print(-m)
+
+#relational
+
+a = 5
+b = 10
+
+c = a <= b
+
+print(c)
+
+#Logical
+
+a = 100
+y = 200
+z = 300
+
+z > x and x > y
+print(z > y)
+print(x > y)
