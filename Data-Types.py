906295d14534082d4895e944ccaf955163ff60cb
Rohit Mahajan <tfrohit02@gmail.com>
2026-03-05 14:11:42 +0530

    add builtin data types example in Data-Types.py

---
+++ Data-Types.py
@@ -0,0 +1,22 @@
+# Builtin data type :
+
+#  1. NONE or NULL
+
+x = None
+print(x)
+
+# 2. Numeric
+# int, Float, Complex, Bool
+
+a = 10 #int
+b = 10.5 #float
+c = 50 + 5j # complex
+d = True #bool
+
+e = 10>5 # will print True as e(10) is more than 5
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(e)
