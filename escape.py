3177791fec34203a65d7ffc6dd805c1f1634a9db
Rohit Mahajan <tfrohit02@gmail.com>
2026-03-05 19:58:31 +0530

    add escape.py file with examples of string manipulation and escape sequences

---
+++ escape.py
@@ -0,0 +1,41 @@
+my_name = "XYZ"
+
+print(my_name)
+
+my_string = "Hello \
+    my name is XYZ \
+    Nice to meet you"
+
+print(my_string)
+
+a = "another \nstring \nto print"
+
+print(a)
+
+my_dir = "C:\Download\\node.js"
+
+print(my_dir)
+
+new = "Jhon said \"Hellow\" to me"
+
+print(new)
+
+# Escape Sequences Examples
+
+# \" - Print double quote
+print("\"Hello\"")
+
+# \' - Print single quote
+print('It\'s a beautiful day')
+
+# \n - Print newline
+print("Line 1\nLine 2\nLine 3")
+
+# \t - Print tab character
+print("Name\tAge\nJohn\t25\nDiana\t28")
+
+# \r - Carriage return
+print("Hello\rWorld")
+
+# \\ - Print backslash
+print("C:\\Users\\Downloads\\file.txt")
